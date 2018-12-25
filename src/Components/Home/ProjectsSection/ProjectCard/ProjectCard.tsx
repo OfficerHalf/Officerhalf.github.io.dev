3123bd49b07d58166fe2ed9b5b3becd1039a0a64
Nathan Smith <nathan@nathan-smith.org>
2018-12-24 22:58:46 -0600

    work

--- src/Components/Home/ProjectsSection/ProjectCard/ProjectCard.tsx
+++ src/Components/Home/ProjectsSection/ProjectCard/ProjectCard.tsx
@@ -10,11 +10,19 @@ export default class ProjectCard extends Component<ProjectCardProps, {}> {
     public render() {
         return (
             <div className="ProjectCardComponent">
-                <div className="name">{this.props.project.name}</div>
-                <div className="description" dangerouslySetInnerHTML={{__html: this.props.project.description}}/>
-                <div className="status">{this.props.project.status}</div>
-                <img className="projectIcon" src={this.props.project.icon} alt=""/>
-                <a href={this.props.project.more}>More Info</a>
+                <div className="wrapper">
+                    <div className="iconSection">
+                        <img className="projectIcon" src={this.props.project.icon} alt=""/>
+                    </div>
+                    <div className="contentSection">
+                        <div className="name">{this.props.project.name}</div>
+                        <div className="description" dangerouslySetInnerHTML={{__html: this.props.project.description}}/>
+                        <div className="bottom">
+                            <span className="status">{this.props.project.status}</span>
+                            <a href={this.props.project.more}>More Info</a>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
