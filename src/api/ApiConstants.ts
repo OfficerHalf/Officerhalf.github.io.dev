c6319e6ddaf42b600585e4777debe413b078f593
Nathan Smith <nathan.smith@bentley.com>
2019-05-23 15:30:56 -0500

    empty

--- src/api/ApiConstants.ts
+++
@@ -1,3 +0,0 @@
-// It's read only anyway. Why are you stealing this?
-export const AwBeans: string = '9ffd3dad4fd54423ad22bc3ce3e1a2fd6bbc9081';
-export const ContentEndpoint: string = 'https://api.buttercms.com/v2/content/';
--- src/pages/Blog/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { RouteComponentProps } from 'react-router-dom';
-
-export default class Blog extends React.PureComponent<RouteComponentProps> {
-    public render() {
-        return <div className="container">Blog</div>;
-    }
-}
