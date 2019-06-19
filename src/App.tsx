801c8ac679acd3aa30fcf9d2a4610755d8134689
Nathan Smith <nathan@nathan-smith.org>
2019-06-19 08:03:16 -0500

    work

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,36 @@
-import React from 'react';
-import { CssBaseline, Container, Toolbar, Typography } from '@material-ui/core';
+import React from "react";
+import {
+  CssBaseline,
+  Container,
+  Toolbar,
+  Typography,
+  IconButton
+} from "@material-ui/core";
+import { GithubCircle, LinkedinBox } from "mdi-material-ui";
+import { Email } from "@material-ui/icons";
 
 const App: React.FC = () => {
-    return (
-        <>
-            <CssBaseline />
-            <Container maxWidth="lg">
-                <Toolbar>
-                    <Typography variant="h5" component="h2">
-                        Nathan Smith
-                    </Typography>
-                </Toolbar>
-            </Container>
-        </>
-    );
+  return (
+    <>
+      <CssBaseline />
+      <Container maxWidth="lg">
+        <Toolbar>
+          <Typography variant="h5" component="h2">
+            Nathan Smith
+          </Typography>
+          <IconButton>
+            <GithubCircle />
+          </IconButton>
+          <IconButton>
+            <LinkedinBox />
+          </IconButton>
+          <IconButton>
+            <Email />
+          </IconButton>
+        </Toolbar>
+      </Container>
+    </>
+  );
 };
 
 export default App;
