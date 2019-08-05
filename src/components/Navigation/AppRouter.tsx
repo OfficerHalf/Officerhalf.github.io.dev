571de3738a4fa5842361d1bf5d3275ef9fcc7da4
Nathan Smith <nathan.smith@bentley.com>
2019-08-05 15:41:03 -0500

    totally changing the look

--- src/components/Navigation/AppRouter.tsx
+++ src/components/Navigation/AppRouter.tsx
@@ -1,21 +1,17 @@
 import * as React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { routes } from '../../constants/routes';
-import { Home } from '../Home';
+import { Home } from '../Home/Home';
 import { Contact } from '../Contact';
 import { FourZeroFour } from '../RootComponents/404';
-import { TopBar } from './TopBar';
 import '../../styles/components/Navigation/Router.scss';
 
 export const Router: React.FC = props => (
-  <>
-    <TopBar />
-    <div className="router-wrapper">
-      <Switch>
-        <Route exact path={routes.app.contact.base} component={Contact} />
-        <Route exact path={routes.app.home.base} component={Home} />
-        <Route component={FourZeroFour} />
-      </Switch>
-    </div>
-  </>
+  <div className="router-wrapper">
+    <Switch>
+      <Route exact path={routes.app.contact.base} component={Contact} />
+      <Route exact path={routes.app.home.base} component={Home} />
+      <Route component={FourZeroFour} />
+    </Switch>
+  </div>
 );
