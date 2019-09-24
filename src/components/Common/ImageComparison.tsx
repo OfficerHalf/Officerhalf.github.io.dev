607442cef3c543d46a02f24fc7d8deffc3553409
Nathan Smith <nathan.smith@bentley.com>
2019-09-24 16:38:55 -0500

    dat image slider is noice

--- src/components/Common/ImageComparison.tsx
+++ src/components/Common/ImageComparison.tsx
@@ -1,11 +1,16 @@
 import * as React from 'react';
 import '../../styles/components/Common/ImageComparison.scss';
+import { Icon } from 'antd';
 
 interface ImageComparisonProps {
   width?: number;
   height?: number;
   imageLeftSrc: string;
   imageRightSrc: string;
+  labelLeft?: string;
+  labelRight?: string;
+  dragHandleColor?: string;
+  dragHandleRadius?: number;
 }
 
 const makeDragImg = (): HTMLImageElement => {
@@ -16,68 +21,153 @@ const makeDragImg = (): HTMLImageElement => {
 };
 
 export const ImageComparison: React.FC<ImageComparisonProps> = props => {
-  const { width = 700, height = 500, imageLeftSrc, imageRightSrc } = props;
-  const dragImgRef = React.useRef<HTMLDivElement>(makeDragImg());
+  const {
+    width = 700,
+    height = 500,
+    imageLeftSrc,
+    imageRightSrc,
+    labelLeft = '',
+    labelRight = '',
+    dragHandleColor = 'white',
+    dragHandleRadius = 40
+  } = props;
   const dragRef = React.useRef<HTMLDivElement>(null);
-  const [startPosition, setStartPosition] = React.useState<number>(0);
+  const animationFrameRef = React.useRef<number>();
   const [dragging, setDragging] = React.useState<boolean>(false);
+  const [startPosition, setStartPosition] = React.useState<number>(0);
   const [position, setPosition] = React.useState<number>(width / 2);
+
+  React.useEffect(() => {
+    return () => {
+      if (animationFrameRef.current) {
+        cancelAnimationFrame(animationFrameRef.current);
+      }
+    };
+  }, []);
 
   React.useEffect(() => {
     setStartPosition(dragRef.current!.getBoundingClientRect().left);
   }, [dragRef]);
 
-  const onDragStart: React.DragEventHandler<HTMLDivElement> = event => {
-    event.dataTransfer.setDragImage(dragImgRef.current, 0, 0);
+  const onDragStart: React.MouseEventHandler<
+    HTMLDivElement
+  > = React.useCallback(() => {
     setDragging(true);
-  };
+  }, []);
 
-  const onDragEnd: React.DragEventHandler<HTMLDivElement> = event => {
-    setDragging(false);
-    setPosition(
-      Math.min(Math.max(0, event.pageX - startPosition + width / 2), width - 3)
-    );
-  };
+  const onDragEnd: React.MouseEventHandler<HTMLDivElement> = React.useCallback(
+    event => {
+      if (dragging === true) {
+        setDragging(false);
+        event.persist();
+        animationFrameRef.current = requestAnimationFrame(() => {
+          setPosition(
+            Math.min(
+              Math.max(
+                dragHandleRadius / 2,
+                event.pageX - dragHandleRadius / 2 - startPosition + width / 2
+              ),
+              width - dragHandleRadius / 2
+            )
+          );
+        });
+      }
+    },
+    [startPosition, width, dragging, dragHandleRadius]
+  );
 
-  const onDrag: React.DragEventHandler<HTMLDivElement> = event => {
-    setPosition(
-      Math.min(Math.max(0, event.pageX - startPosition + width / 2), width - 3)
-    );
-  };
-
-  const onDragOver: React.DragEventHandler<HTMLDivElement> = event => {
-    event.preventDefault();
-  };
+  const onDrag: React.MouseEventHandler<HTMLDivElement> = React.useCallback(
+    event => {
+      if (dragging === true) {
+        event.persist();
+        animationFrameRef.current = requestAnimationFrame(() => {
+          setPosition(
+            Math.min(
+              Math.max(
+                dragHandleRadius / 2,
+                event.pageX - dragHandleRadius / 2 - startPosition + width / 2
+              ),
+              width - dragHandleRadius / 2
+            )
+          );
+        });
+      }
+    },
+    [startPosition, width, dragging, dragHandleRadius]
+  );
 
   return (
-    <div style={{ height, width }}>
+    <div
+      className="image-comparison-container"
+      onMouseMove={onDrag}
+      onMouseUp={onDragEnd}
+      onMouseLeave={onDragEnd}
+      draggable={false}
+    >
       <div
-        className="image-comparison-left"
+        draggable={false}
+        className="image-comparison-wrapper"
         style={{
-          width: position,
-          backgroundImage: `url("${imageLeftSrc}")`
+          width: position
         }}
-      />
+      >
+        <div
+          draggable={false}
+          className="image-comparison-left"
+          style={{
+            width: position,
+            height: height,
+            backgroundImage: `url("${imageLeftSrc}")`,
+            backgroundSize: `${width}px ${height}px`
+          }}
+        />
+        <div draggable={false} className="image-comparison-left-label">
+          {labelLeft}
+        </div>
+      </div>
       <div
-        className="image-comparison-right"
+        draggable={false}
+        className="image-comparison-wrapper"
         style={{
-          width: width - position,
-          backgroundImage: `url("${imageRightSrc}")`,
-          backgroundSize: `${width}px ${height}px`
+          width: width - position
         }}
-      />
+      >
+        <div
+          draggable={false}
+          className="image-comparison-right"
+          style={{
+            width: width - position,
+            height: height,
+            backgroundImage: `url("${imageRightSrc}")`,
+            backgroundSize: `${width}px ${height}px`
+          }}
+        />
+        <div draggable={false} className="image-comparison-right-label">
+          {labelRight}
+        </div>
+      </div>
       <div
+        className="image-comparison-draggable-container"
         style={{
-          transform: `translateX(${position}px)`
+          height,
+          transform: `translateX(${position - dragHandleRadius / 2}px)`
         }}
-        draggable
-        onDragStart={onDragStart}
-        onDragEnd={onDragEnd}
-        onDragOver={onDragOver}
-        onDrag={onDrag}
-        ref={dragRef}
-        className="image-comparison-draggable"
-      />
+      >
+        <div
+          style={{
+            width: dragHandleRadius,
+            height: dragHandleRadius,
+            borderRadius: dragHandleRadius / 2,
+            backgroundColor: dragHandleColor
+          }}
+          onMouseDown={onDragStart}
+          ref={dragRef}
+          className="image-comparison-draggable"
+        >
+          <Icon className="image-comparison-draggable-icon" type="left" />
+          <Icon className="image-comparison-draggable-icon" type="right" />
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Projects/Alloy.tsx
+++ src/components/Projects/Alloy.tsx
@@ -1,12 +1,33 @@
 import * as React from 'react';
 import { RouteComponentProps, withRouter } from 'react-router-dom';
-import { PageHeader } from 'antd';
+import { PageHeader, Layout } from 'antd';
+import { ImageComparison } from '../Common/ImageComparison';
+
+const { Header, Content, Footer } = Layout;
 
 const Alloy: React.FC<RouteComponentProps> = props => {
   return (
-    <>
+    <Layout>
       <PageHeader onBack={() => props.history.goBack()} title="Alloy" />
-    </>
+      <Content style={{ justifyContent: 'center', display: 'flex' }}>
+        <div>
+          <ImageComparison
+            imageLeftSrc={`${process.env.PUBLIC_URL}/Alloy2.png`}
+            imageRightSrc={`${process.env.PUBLIC_URL}/Monokai2.png`}
+            width={1440}
+            height={900}
+            labelLeft="Alloy"
+            labelRight="Monokai"
+            dragHandleColor="#F0558E"
+          />
+          <h2>About</h2>
+          <p>
+            Alloy is my response to the green and yellow tinted world of
+            Monokai.
+          </p>
+        </div>
+      </Content>
+    </Layout>
   );
 };
 
