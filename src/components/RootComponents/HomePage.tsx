c54aa8827e3ef1d00b808dc97cbbb95a1e2c719b
Nathan Smith <nathan.smith@bentley.com>
2020-05-20 17:13:38 -0500

    bunch of new content, plus syntax highlighting

--- src/components/RootComponents/HomePage.tsx
+++ src/components/RootComponents/HomePage.tsx
@@ -4,9 +4,11 @@ import { postList } from '../../util/cms';
 import { BlogPost } from '../../types/cms';
 import { Post } from '../Blog/Post';
 import { css, jsx } from '@emotion/core';
+import theme from '../../util/theme';
 
 const postListStyle = css`
   max-width: 900px;
+  margin: ${theme.space.xl};
 `;
 
 export const HomePage: React.FC = props => {
--- src/hooks/usePrismjs.tsx
+++ src/hooks/usePrismjs.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import Prismjs from 'prismjs';
+
 import '../util/prismjs-alloy.scss';
+import 'prismjs/plugins/line-numbers/prism-line-numbers.css';
+
+require('prismjs/components/prism-typescript');
+require('prismjs/components/prism-jsx');
+require('prismjs/components/prism-tsx');
 require('prismjs/plugins/line-numbers/prism-line-numbers');
 
 export function usePrismjs<T extends HTMLElement>(
