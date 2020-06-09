8bd8c11c33a019f8656e7bc556e32354e143a10e
Nathan Smith <nathan@nathan-smith.org>
2020-06-08 20:39:21 -0500

    added dropdown and alloy project page

--- src/components/Navigation/Header.tsx
+++ src/components/Navigation/Header.tsx
@@ -1,8 +1,11 @@
 /** @jsx jsx */
+import React from 'react';
 import { css, jsx } from '@emotion/core';
 import { theme } from '../../util/theme';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { Title } from '../Typography';
+import { Dropdown } from '../Common/Dropdown';
+import { routes } from '../../util/routes';
 
 const { color, space } = theme;
 
@@ -10,46 +13,49 @@ const headerStyles = css`
   background-color: ${color.primary};
   color: white;
   padding: ${space.m};
-  ul {
-    display: flex;
-    list-style: none;
-    margin: 0;
-    padding: 0;
-    li {
-      display: block;
-      margin: 0 ${space.sm};
-      cursor: pointer;
-      &:first-child {
-        margin-left: 0;
-      }
-      &:last-child {
-        margin-right: 0;
-      }
+`;
 
-      &.spacer {
-        cursor: initial;
-        flex-grow: 1;
+const listStyle = css`
+  display: flex;
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  & > li {
+    display: block;
+    margin: 0 ${space.sm};
+    cursor: pointer;
+    &:first-child {
+      margin-left: 0;
+    }
+    &:last-child {
+      margin-right: 0;
+    }
+
+    &.spacer {
+      cursor: initial;
+      flex-grow: 1;
+    }
+    a {
+      text-decoration: none;
+      &,
+      &:active,
+      &:hover,
+      &:visited {
+        color: white;
       }
-      a {
-        text-decoration: none;
-        &,
-        &:active,
-        &:hover,
-        &:visited {
-          color: white;
-        }
-        &:hover {
-          text-decoration: underline;
-        }
+      &:hover {
+        text-decoration: underline;
       }
     }
   }
 `;
 
 export const Header: React.FC = props => {
+  const navigate = useNavigate();
+
   return (
     <header css={headerStyles}>
-      <ul>
+      <ul css={listStyle}>
         <li>
           <Link to="/">
             <Title>Nathan Smith</Title>
@@ -57,12 +63,24 @@ export const Header: React.FC = props => {
         </li>
         <li className="spacer" />
         <li>
-          <Link to="/about">
+          <Dropdown
+            options={[
+              {
+                text: 'Alloy',
+                onClick: () => navigate(routes.project.alloy.link)
+              }
+            ]}
+            arrow>
+            <Title>Projects</Title>
+          </Dropdown>
+        </li>
+        <li>
+          <Link to={routes.about}>
             <Title>About</Title>
           </Link>
         </li>
         <li>
-          <Link to="/contact">
+          <Link to={routes.contact}>
             <Title>Contact</Title>
           </Link>
         </li>
