2db52aad840ce46344eeb74b5a401749972c1ac5
Nathan Smith <nathan@nathan-smith.org>
2020-07-24 17:03:42 -0500

    trying without transition group

--- src/components/Navigation/Drawer.tsx
+++ src/components/Navigation/Drawer.tsx
@@ -1,7 +1,6 @@
 /** @jsx jsx */
 import React from 'react';
 import { css, jsx } from '@emotion/core';
-import { CSSTransition } from 'react-transition-group';
 import { MenuItem } from '../../../types/nav';
 import { Close, Adjust } from '../Icons';
 import { staticTheme } from '../../util/theme';
@@ -9,6 +8,7 @@ import { DrawerItem } from './DrawerItem';
 import { ToggleSwitch } from '../Common/ToggleSwitch';
 import { ThemeContext } from '../../store/ThemeContext';
 import { Portal } from '../Common/Portal';
+import cx from 'classnames';
 
 const { space, elevation } = staticTheme;
 
@@ -18,87 +18,28 @@ interface DrawerProps {
   items: MenuItem[];
 }
 
-const drawerTransitionStyle = css`
-  opacity: 0;
-  display: none;
-
-  &.appear {
-    display: block;
-    .content {
-      transform: translateX(100%);
-    }
-  }
-  &.appear-active {
-    display: block;
-    opacity: 1;
-    transition: opacity 300ms ease-in-out;
-    .content {
-      transform: translateX(0%);
-      transition: transform 300ms ease-in-out;
-    }
-  }
-  &.enter {
-    display: block;
-    .content {
-      transform: translateX(100%);
-    }
-  }
-  &.enter-active {
-    display: block;
-    opacity: 1;
-    transition: opacity 300ms ease-in-out;
-    .content {
-      transform: translateX(0%);
-      transition: transform 300ms ease-in-out;
-    }
-  }
-  &.enter-done {
-    display: block;
-    opacity: 1;
-    .content {
-      transform: translateX(0%);
-    }
-  }
-  &.exit {
-    display: block;
-    opacity: 1;
-    .content {
-      transform: translateX(0%);
-    }
-  }
-  &.exit-active {
-    display: block;
-    opacity: 0;
-    transition: opacity 300ms ease-in-out;
-    .content {
-      transform: translateX(100%);
-      transition: transform 300ms ease-in-out;
-    }
-  }
-  &.exit-done {
-    .content {
-      transform: translateX(100%);
-    }
-  }
-`;
-
 export const Drawer: React.FC<DrawerProps> = props => {
   const { open, onClose, items, ...rest } = props;
+  const [show, setShow] = React.useState<boolean>(open);
   const { dark, theme, toggleTheme } = React.useContext(ThemeContext);
   const { textColor, background, primary } = theme;
 
-  const handleThemeToggle = React.useCallback(() => {
-    onClose();
-    toggleTheme();
-  }, [onClose, toggleTheme]);
+  React.useEffect(() => {
+    setShow(open);
+  }, [open]);
 
   const drawerStyle = css`
     height: 100%;
     width: 100%;
     position: fixed;
     top: 0;
+    left: 0;
     background-color: rgba(0, 0, 0, 0.2);
-    ${drawerTransitionStyle};
+    display: none;
+    overflow: hidden;
+    &.open {
+      display: block;
+    }
     .content {
       width: 75%;
       height: 100%;
@@ -106,6 +47,10 @@ export const Drawer: React.FC<DrawerProps> = props => {
       background-color: ${dark ? background.background10 : background.background};
       float: right;
       box-shadow: ${elevation[4]};
+      transition: transform 300ms ease-in-out;
+      &.show {
+        transform: translateX(0%);
+      }
     }
   `;
 
@@ -129,30 +74,28 @@ export const Drawer: React.FC<DrawerProps> = props => {
 
   return (
     <Portal>
-      <CSSTransition in={open} appear timeout={300} nodeRef={drawerRef}>
-        <div ref={drawerRef} className="shade" css={drawerStyle} onClick={onClose}>
-          <div className="content" {...rest} onClick={e => e.stopPropagation()}>
-            <div css={closeContainerStyle}>
-              <Close css={closeStyle} onClick={onClose} />
-            </div>
-            <div css={closeContainerStyle}>
-              <Adjust
-                css={css`
-                  height: ${space.l};
-                  width: ${space.l};
-                  margin-right: ${space.s};
-                  fill: ${dark ? primary.lighter : background.background20};
-                  transform: rotate(${dark ? '0deg' : '180deg'});
-                `}
-              />
-              <ToggleSwitch checked={dark} onChange={handleThemeToggle} />
-            </div>
-            {items.map(i => (
-              <DrawerItem key={i.id || i.text} {...i} onClose={onClose} />
-            ))}
+      <div ref={drawerRef} className={cx('shade', { open })} css={drawerStyle} onClick={onClose}>
+        <div className={cx('content', { show })} {...rest} onClick={e => e.stopPropagation()}>
+          <div css={closeContainerStyle}>
+            <Close css={closeStyle} onClick={onClose} />
           </div>
+          <div css={closeContainerStyle}>
+            <Adjust
+              css={css`
+                height: ${space.l};
+                width: ${space.l};
+                margin-right: ${space.s};
+                fill: ${dark ? primary.lighter : background.background20};
+                transform: rotate(${dark ? '0deg' : '180deg'});
+              `}
+            />
+            <ToggleSwitch checked={dark} onChange={toggleTheme} />
+          </div>
+          {items.map(i => (
+            <DrawerItem key={i.id || i.text} {...i} onClose={onClose} />
+          ))}
         </div>
-      </CSSTransition>
+      </div>
     </Portal>
   );
 };
