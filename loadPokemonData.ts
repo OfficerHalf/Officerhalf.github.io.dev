7cbbf5ff2df3a8f34337c9a96089191417e83595
Nathan Smith <nathan.smith@bentley.com>
2020-08-29 15:37:20 -0500

    update pokemon scripts

--- loadPokemonData.ts
+++ loadPokemonData.ts
@@ -2,26 +2,49 @@ import { listAll, getOne, getIdFromRel } from './src/util/pokemon';
 import { Pokemon, PokemonSpecies } from './types/pokemon';
 import { promises } from 'fs';
 
+const filePath = './data/Pokemon.json';
+
+async function loadData(): Promise<{ pokemon: { [id: string]: Pokemon }; species: { [id: string]: PokemonSpecies } }> {
+  const content = await promises.readFile(filePath, { encoding: 'utf8' });
+  return JSON.parse(content);
+}
+
+async function saveData(pokemon: { [id: string]: Pokemon }, species: { [id: string]: PokemonSpecies }): Promise<void> {
+  await promises.writeFile(filePath, JSON.stringify({ pokemon, species }));
+}
+
 async function main() {
+  let pokemonCollection: { [id: string]: Pokemon } = {};
+  let speciesCollection: { [id: string]: PokemonSpecies } = {};
+
   // List all pokemon
   const allPokemon = await listAll();
 
-  // Get all pokemon + species
-  const pokemonCollection: { [id: string]: Pokemon } = {};
-  const speciesCollection: { [id: string]: PokemonSpecies } = {};
+  const _start = process.argv[2] || '0';
+  const start = parseInt(_start);
+  const _end = process.argv[3] || getIdFromRel(allPokemon[allPokemon.length - 1]).toString();
+  const end = parseInt(_end);
 
+  // See if we're only getting a subset
+  if (start > 0 || end > 0) {
+    const resp = await loadData();
+    pokemonCollection = resp.pokemon;
+    speciesCollection = resp.species;
+  }
+
+  // Get all pokemon + species
   for (let i = 0; i < allPokemon.length; i++) {
     const id = getIdFromRel(allPokemon[i]);
-    const { pokemon, species } = await getOne(id);
-    pokemonCollection[pokemon.id] = pokemon;
-    speciesCollection[species.id] = species;
+    if (id >= start && id <= end) {
+      const { pokemon, species } = await getOne(id);
+      pokemonCollection[pokemon.id] = pokemon;
+      speciesCollection[species.id] = species;
+      console.log(`Got ${pokemon.name}.`);
+    }
   }
 
   // Write to file
-  await promises.writeFile(
-    './data/Pokemon.json',
-    JSON.stringify({ pokemon: pokemonCollection, species: speciesCollection })
-  );
+  await saveData(pokemonCollection, speciesCollection);
 }
 
 main();
