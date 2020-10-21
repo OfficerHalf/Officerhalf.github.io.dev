ed5663a8d83dbf178bbe5a376ba8e1e8e4ca0f2b
Nathan Smith <nathan.smith@bentley.com>
2020-10-21 13:55:53 -0500

    it works

--- ng/src/app/snake/snake.component.ts
+++ ng/src/app/snake/snake.component.ts
@@ -1,6 +1,6 @@
 import { Component, HostListener, OnInit } from '@angular/core';
-import { Board, BoardRow, Direction, Snake, UI } from 'src/models/snake';
-import { createBoard, move as doMove } from 'src/utils/snake';
+import { Board, BoardRow, Direction, Snake, UI, Position } from 'src/models/snake';
+import { createBoard, move as doMove, randomChoice } from 'src/utils/snake';
 
 @Component({
   selector: 'app-snake',
@@ -12,27 +12,65 @@ export class SnakeComponent implements OnInit {
   snake: Snake;
   direction: Direction;
   running: number;
+  xSize: number;
+  ySize: number;
+  speedMs: number;
+  minSpeedMs: number;
+  lost: boolean;
 
   constructor() {}
 
   ngOnInit(): void {
-    const size = 10;
-    this.board = createBoard(size, size);
-    const start = Math.floor(size / 2);
-    this.snake = [{ x: start, y: start }];
+    this.lost = false;
+    this.xSize = 20;
+    this.ySize = 20;
+    this.speedMs = 1000;
+    this.minSpeedMs = 500;
+    this.board = createBoard(this.xSize, this.ySize);
+    const startX = Math.floor(this.xSize / 2);
+    const startY = Math.floor(this.ySize / 2);
+    this.snake = [{ x: startX, y: startY }];
+    this.board[this.snake[0].y][this.snake[0].x] = UI.snake;
+    this.placeFood();
     this.direction = Math.floor(Math.random() * 4);
-    window.addEventListener('keydown', event => this.changeDirection(event));
   }
 
   move() {
+    // Find next position
     const newHead = doMove(this.snake[0], this.direction);
-    this.snake.unshift(newHead);
-    this.snake.pop();
+    let grow = false;
+
+    // Check to see what's at the next position
+    if (newHead.x < 0 || newHead.y < 0 || newHead.x >= this.xSize || newHead.y >= this.ySize) {
+      this.lost = true;
+    } else if (this.board[newHead.y][newHead.x] === UI.snake) {
+      this.lost = true;
+    } else if (this.board[newHead.y][newHead.x] === UI.food) {
+      grow = true;
+    }
+
+    if (!this.lost) {
+      // Move
+      this.snake.unshift(newHead);
+      this.board[newHead.y][newHead.x] = UI.snake;
+
+      // Grow / move
+      if (!grow) {
+        const oldTail = this.snake.pop();
+        this.board[oldTail.y][oldTail.x] = '';
+      } else {
+        this.speedMs -= 50;
+        this.placeFood();
+        this.start();
+      }
+    } else {
+      this.stop();
+    }
   }
 
   start() {
     this.stop();
-    this.running = window.setInterval(() => this.move(), 1000);
+    this.running = window.setInterval(() => this.move(), this.speedMs);
   }
 
   stop() {
@@ -41,23 +79,47 @@ export class SnakeComponent implements OnInit {
     }
   }
 
+  placeFood(count: number = 1) {
+    const choices: Position[] = [];
+    for (let y = 0; y < this.ySize; y++) {
+      for (let x = 0; x < this.xSize; x++) {
+        if (this.board[y][x] === '') {
+          choices.push({ x, y });
+        }
+      }
+    }
+    const positions = randomChoice(choices);
+    positions.forEach(p => {
+      this.board[p.y][p.x] = UI.food;
+    });
+  }
+
+  @HostListener('document:keydown', ['$event'])
   changeDirection(event: KeyboardEvent) {
     switch (event.key) {
       case 'Down': // IE/Edge specific value
       case 'ArrowDown':
-        this.direction = Direction.South;
+        if (this.direction !== Direction.North) {
+          this.direction = Direction.South;
+        }
         break;
       case 'Up': // IE/Edge specific value
       case 'ArrowUp':
-        this.direction = Direction.North;
+        if (this.direction !== Direction.South) {
+          this.direction = Direction.North;
+        }
         break;
       case 'Left': // IE/Edge specific value
       case 'ArrowLeft':
-        this.direction = Direction.West;
+        if (this.direction !== Direction.East) {
+          this.direction = Direction.West;
+        }
         break;
       case 'Right': // IE/Edge specific value
       case 'ArrowRight':
-        this.direction = Direction.East;
+        if (this.direction !== Direction.West) {
+          this.direction = Direction.East;
+        }
         break;
       default:
         return;
--- ng/src/app/snake/snake.module.ts
+++ ng/src/app/snake/snake.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SnakeComponent } from './snake.component';
+import { BoardComponent } from './board/board.component';
+import { SquareComponent } from './square/square.component';
 
 
 
 @NgModule({
-  declarations: [SnakeComponent],
+  declarations: [SnakeComponent, BoardComponent, SquareComponent],
   imports: [
     CommonModule
   ]
