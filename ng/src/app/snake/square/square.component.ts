df1fa16abe1bc978e0a37cf9b5bae92f85ca5cad
Nathan Smith <nathan.smith@bentley.com>
2020-10-26 12:50:50 -0500

    add some color to it

--- ng/src/app/snake/square/square.component.ts
+++ ng/src/app/snake/square/square.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { UI } from 'src/models/snake';
 
 @Component({
   selector: 'app-square',
@@ -8,7 +9,9 @@ import { Component, OnInit, Input } from '@angular/core';
 export class SquareComponent implements OnInit {
   @Input() value: string;
 
-  constructor() {}
-
   ngOnInit(): void {}
+
+  get status(): string {
+    return this.value === UI.snake ? 'snake' : this.value === UI.food ? 'food' : 'empty';
+  }
 }
