c1d263c4d0a78267d6b0a524c69920e03a6f6338
Nathan Smith <nathan@nathan-smith.org>
2020-11-27 23:41:06 -0600

    work

--- ng/src/app/ancestry-and-culture/ancestry-and-culture.module.ts
+++ ng/src/app/ancestry-and-culture/ancestry-and-culture.module.ts
@@ -1,60 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 // Material
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { LayoutModule } from '@angular/cdk/layout';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
-import { MatStepperModule } from '@angular/material/stepper';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
 
-import { AncestryAndCultureComponent } from './ancestry-and-culture.component';
-import { AncestryAndCultureService } from './ancestry-and-culture.service';
-import { AncestryComponent } from './ancestry/ancestry.component';
-import { CultureComponent } from './culture/culture.component';
-import { AbilityComponent } from './ability/ability.component';
-import { AncestryStepComponent } from './ancestry-step/ancestry-step.component';
-import { CultureStepComponent } from './culture-step/culture-step.component';
-import { GenerateRaceStepComponent } from './generate-race-step/generate-race-step.component';
-import { TraitDisplayComponent } from './trait-display/trait-display.component';
 import { AncestryAndCultureRoutingModule } from './ancestry-and-culture-routing.module';
+import { AncestryAndCultureComponent } from './ancestry-and-culture.component';
 
 @NgModule({
-  declarations: [
-    AncestryAndCultureComponent,
-    AncestryComponent,
-    CultureComponent,
-    AbilityComponent,
-    AncestryStepComponent,
-    CultureStepComponent,
-    GenerateRaceStepComponent,
-    TraitDisplayComponent
-  ],
-  imports: [
-    CommonModule,
-    ReactiveFormsModule,
-    AncestryAndCultureRoutingModule,
-    MatAutocompleteModule,
-    MatInputModule,
-    MatSelectModule,
-    MatToolbarModule,
-    LayoutModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule,
-    MatStepperModule,
-    MatGridListModule,
-    MatDividerModule
-  ],
-  providers: [AncestryAndCultureService]
+  declarations: [AncestryAndCultureComponent],
+  imports: [CommonModule, AncestryAndCultureRoutingModule, MatInputModule, MatIconModule, FormsModule, MatButtonModule]
 })
 export class AncestryAndCultureModule {}
