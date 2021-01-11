c8e36ecc0e11a6a3960a1a2c08cdd2a52bf8dcfe
Nathan Smith <nathan@nathan-smith.org>
2021-01-11 16:05:48 -0600

    init

---
+++ ng-site/src/typings.d.ts
@@ -0,0 +1,31 @@
+import { KeyboardEventHandler, MouseEventHandler } from 'react';
+
+interface StickyAlert {
+  content: string;
+  title?: string;
+  alertType?: 'alert-primary' | 'alert-success' | 'alert-secondary' | 'alert-danger';
+  fillType?: 'filled-lm' | 'filled-dm' | 'filled';
+  hasDismissButton?: boolean;
+  timeShown?: number;
+}
+
+interface Halfmoon {
+  pageWrapper: HTMLElement;
+  stickyAlerts: HTMLElement;
+  darkModeOn: boolean;
+  createCookie: (cookieName: string, cookieValue: string, days: number) => void;
+  readCookie: (cookieName: string) => string | null;
+  eraseCookie: (cookieName: string) => void;
+  toggleDarkMode: () => void;
+  getPreferredMode: () => 'light-mode' | 'dark-mode' | 'not-set';
+  toggleSidebar: () => void;
+  deactivateAllDropdownToggles: () => void;
+  toggleModal: (modalId: string) => void;
+  toastAlert: (alertId: string, timeShown: number) => void;
+  initStickyAlert: (alert: StickyAlert) => void;
+  clickHandler: MouseEventHandler;
+  keydownHandler: KeyboardEventHandler;
+}
+
+declare const halfmoonOnDOMContentLoaded: () => void;
+declare const halfmoon: Halfmoon;
