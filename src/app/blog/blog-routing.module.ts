8457430d430da92cce2573986df13272acf1c2ec
Nathan Smith <nsmith1@onetrust.com>
2021-07-20 16:35:04 -0400

    add some good base theming

--- src/app/blog/blog-routing.module.ts
+++ src/app/blog/blog-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BlogPostComponent } from './components/blog-post/blog-post.component';
-import { BlogComponent } from './components/blog/blog.component';
+import { BlogComponent } from './blog.component';
 
 const routes: Routes = [
   {
--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -1,5 +1,5 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
-import { BlogService } from '../../services/blog.service';
+import { BlogService } from './services/blog.service';
 
 @Component({
   selector: 'app-blog',
