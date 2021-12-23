3672519a68653b609d12eff39a4e9d5faf9563d3
Nathan Smith <nsmith1@onetrust.com>
2021-12-23 10:27:34 -0500

    reset to blank slate

--- src/environments/environment.interface.ts
+++
@@ -1,4 +0,0 @@
-export interface DevSiteEnvironment {
-  production: boolean;
-  butter: string;
-}
--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-import { DevSiteEnvironment } from './environment.interface';
-
-export const environment: DevSiteEnvironment = {
-  production: true,
-  butter: '9ffd3dad4fd54423ad22bc3ce3e1a2fd6bbc9081'
-};
