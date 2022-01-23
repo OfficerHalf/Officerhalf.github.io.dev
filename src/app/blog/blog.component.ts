93e0a67507541d6ef87409d3ced68e400e703545
OfficerHalf <nathan@nathan-smith.org>
2022-01-22 20:27:27 -0500

    feat: display featured images

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -1,4 +1,5 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { BlogPost } from './interfaces/blog.interface';
 import { BlogService } from './services/blog.service';
 
 @Component({
@@ -11,4 +12,11 @@ export class BlogComponent {
   blogPosts = this.blogService.getPostList();
 
   constructor(private readonly blogService: BlogService) {}
+
+  featuredImage(post: BlogPost): string | null {
+    if (post.featured_image) {
+      return `url(${post.featured_image})`;
+    }
+    return null;
+  }
 }
