8b56605ba919247110294ad44dd8c18169df333e
Nathan Smith <nsmith1@onetrust.com>
2022-05-11 14:22:21 -0400

    refactor: clean repo

--- src/components/Icons/ChevronLeft.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export const ChevronLeft = React.forwardRef<SVGSVGElement, React.SVGProps<SVGSVGElement>>((props, ref) => (
-  <svg ref={ref} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" {...props}>
-    <path d="M7.05 9.293L6.343 10 12 15.657l1.414-1.414L9.172 10l4.242-4.243L12 4.343z" />
-  </svg>
-));
