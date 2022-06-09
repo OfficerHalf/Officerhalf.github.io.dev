e6c24e1f105034e1dbfa4f7be36a13df3c028cc1
Nathan Smith <nathan@nathan-smith.org>
2022-06-09 18:50:03 -0400

    adding resume route, no styles or complete content yet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,29 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "resume",
+    loadChildren: () =>
+      import("./resume/resume.module").then((m) => m.ResumeModule),
+  },
+  {
+    path: "home",
+    loadChildren: () => import("./home/home.module").then((m) => m.HomeModule),
+  },
+  {
+    path: "",
+    pathMatch: "full",
+    redirectTo: "home",
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {
-    initialNavigation: 'enabledBlocking'
-})],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes, {
+      initialNavigation: "enabledBlocking",
+    }),
+  ],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
