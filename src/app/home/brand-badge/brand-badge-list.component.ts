fa1fc93717c452fd34f599922a74d80eb48cde88
Nathan Smith <nathan@payrollintegrations.com>
2023-07-24 23:57:57 -0400

    use self-closing tags where possible

--- src/app/home/brand-badge/brand-badge-list.component.ts
+++ src/app/home/brand-badge/brand-badge-list.component.ts
@@ -1,9 +1,9 @@
 import { Component } from "@angular/core";
 
 @Component({
-    selector: "app-brand-badge-list",
-    template: "<ng-content></ng-content>",
-    styleUrls: ["./brand-badge-list.component.scss"],
-    standalone: true,
+  selector: "app-brand-badge-list",
+  template: "<ng-content/>",
+  styleUrls: ["./brand-badge-list.component.scss"],
+  standalone: true,
 })
 export class BrandBadgeListComponent {}
